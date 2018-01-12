4548a9bece1ce8aab9c9715e261324d944edef28
Kyra Post <kpost97@dhcp-18-189-30-15.dyn.mit.edu>
2018-01-12 14:12:43 -0500

    changed timeout

--- arduino_extension.js
+++ arduino_extension.js
@@ -352,6 +352,15 @@
     device.send(msg.buffer);
   }
 
+  function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
+
   ext.whenConnected = function() {
     if (notifyConnection) return true;
     return false;
@@ -426,10 +435,13 @@
   ext.moveForward = function(time) {
     rotateServo(leftservo, Math.round(90*(1+speed)));
     rotateServo(rightservo, Math.round(90*(1+speed)));
-    setTimeout(function() {
+    sleep(time*1000);
+    rotateServo(leftservo, 90);
+    rotateServo(rightservo, 85);
+    /*setTimeout(function() {
       rotateServo(leftservo, 90);
       rotateServo(rightservo, 85);
-    }, time*1000);
+    }, time*1000);*/
 
   };
 
