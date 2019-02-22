8d271c9bb1a17728e3ac7ec0d0b83c0a71b4127b
Randi Williams <randi.w.2012@gmail.com>
2019-02-22 00:25:03 -0500

    getting plugin to communicate with js

--- Chromebook/arduino_extension.js
+++ Chromebook/arduino_extension.js
@@ -92,6 +92,39 @@
   var rgb = [0, 0, 0];
   var colorMap = {'white':[0,0,0], 'red':[0,255,255], 'orange':[0,95,255], 'yellow':[15,70,255], 'green':[255,0,255], 'blue':[255,255,0], 'purple':[60,255,60], 'pink':[60,255,90]};
 
+/* from SNAP */
+var extensionId = 'meajklokhjoflbimamdbhpdjlondmgpi',
+    postal = new Postal(),
+    firmata = {
+        Board: function(port, callback) {
+            chrome.runtime.sendMessage(extensionId, { command: 'connectBoard', args: [ port ] }, callback)
+        }
+    },
+    require = function () {};
+
+// Messaging between web client and plugin
+
+function Postal() {};
+
+// Command sender function factory
+Postal.prototype.commandSender = function () {
+    var myself = this,
+        command = arguments[0],
+        args = Array.from(arguments).slice(1),
+        callback = typeof args[args.length - 1] === 'function' ? args.splice(args.length - 1) : null;
+
+    return function () { myself.sendCommand(command, args, callback); };
+};
+
+Postal.prototype.sendCommand = function (command, args, callback) {
+    chrome.runtime.sendMessage(extensionId, { command: command, args: args }, callback);
+};
+
+chrome.serial = {
+    getDevices: function (callback) { postal.sendCommand('getDevices', null, callback) }
+};
+/* FROM SNAP */
+
   var hwList = new HWList();
 
   function HWList() {
