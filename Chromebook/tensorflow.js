50e2f59955a40928ecb04c0fbe6eda458f5eced9
Randi Williams <randi.w.2012@gmail.com>
2020-01-02 20:49:55 -0500

    Removing imports for now

--- Chromebook/tensorflow.js
+++ Chromebook/tensorflow.js
@@ -1,5 +1,5 @@
-import * as tf from '@tensorflow/tfjs';
-import * as tmImage from '@teachablemachine/image';
+//import * as tf from '@tensorflow/tfjs';
+//import * as tmImage from '@teachablemachine/image';
 
 (function(ext) {
 
