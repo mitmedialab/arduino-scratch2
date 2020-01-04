9eb9a148fc61be851053f1f80ac9588115f40502
Randi Williams <randi.w.2012@gmail.com>
2020-01-03 23:00:48 -0500

    Trying webcam using combination of dalton and clarifai code

--- Chromebook/tensorflow.js
+++ Chromebook/tensorflow.js
@@ -1,168 +1,104 @@
-//import * as tf from '@tensorflow/tfjs';
-//import * as tmImage from '@teachablemachine/image';
+/* Extension using the JavaScript Speech API for text to speech */
+/* Sayamindu Dasgupta <sayamindu@media.mit.edu>, April 2014 */
 
-(function(ext) {
+new (function() {
+    var ext = this;
+    var videoElem = undefined;
+    var extStatus = 1;
+    /*
+     * variables
+     */
 
-  var _selectors = {};
-  
-  var video;
-  var webcamStream;
-  const URL = "https://teachablemachine.withgoogle.com/models/PU-63hTh/";
-  let model, webcam, labelContainer, maxPredictions;
-
-  
-  function appendBuffer( buffer1, buffer2 ) {
-    var tmp = new Uint8Array( buffer1.byteLength + buffer2.byteLength );
-    var i;
-    
-    for (i = 0; i < buffer1.byteLength; i++) {
-      tmp[i] = buffer1[i];
-    }
-    for (var j = 0; j < buffer2.byteLength; j++) {
-      tmp[i] = buffer2[j];
-      i++;
-    }
-    return tmp.buffer;
-}
-  
-      // Load the image model and setup the webcam
-    ext.loadModel = async function() {
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
-
-        // load the model and metadata
-        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-        // or files from your local hard drive
-        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-        model = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
-    }
-  
-  ext.startImageWebcam = function() {
-    console.log('Starting Webcam');
-    if (navigator.getUserMedia) {
-       navigator.getUserMedia (
-
-          // constraints
+    async function startImageWebcam() {
+      console.log("Starting webcam");
+      if (navigator.getUserMedia) {
+        extStatus = 2;
+        navigator.getUserMedia (
+          // options
           {
-             video: true,
-             audio: false
+            video: true,
+            audio: false
           },
 
-          // successCallback
+          // success callback
           function(localMediaStream) {
-            console.log('Success in opening media stream: ' + localMediaStream);
-            video = document.createElement('video');
-            video.srcObject = localMediaStream;
-            webcamStream = localMediaStream;
-            console.log('webcamStream: ' + webcamStream);
+            videoElem = document.createElement('video');
+            videoElem.src = window.URL.createObjectURL(localMediaStream);
+            // need to call videoElem.play()?
+            window.webcamStream = localMediaStream;
           },
 
-          // errorCallback
+          // error callback
           function(err) {
-             console.log("The following error occured: " + err);
+            console.log("Error starting webcam: " + err);
           }
        );
-    } else {
-       console.log("getUserMedia not supported");
-    }  
-      }
-
-  ext.stopWebcam = function() {
-    console.log('Stopping media stream');
-    console.log('Webcamstream: ' + webcamstream);
-          webcamStream.getVideoTracks().forEach(function(track) {
-            track.stop();
-          });
-      }
-
-
-
-     ext.getCameraImage = function(callback) {
-      console.log('in getCameraImage');
-        
-        
-        //---------------------
-        // TAKE A SNAPSHOT CODE
-        //---------------------
-        canvas = document.createElement('canvas');
-        canvas.width = 32;
-        canvas.height = 32;
-        ctx = canvas.getContext('2d');
-        console.log('Canvas: ' + canvas);
-    
-         // Draws current image from the video element into the canvas
-        console.log('video: ' + video);
-        ctx.drawImage(video, 0,0, canvas.width, canvas.height);
-        // turn canvas into png and save it -- HANGS FOREVER
-        var img    = ctx.toDataURL("image/png");
-        console.log('Img: ' + img);
-        
-        // get image data into var
-        img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        //console.log('imgData: ' + img_data.data);
-        callback(img_data);
-        
-      }
-      
-      // run the webcam image through the image model
-    ext.predictImage =  async function() { 
-        // predict can take in an image, video or canvas html element
-        console.log("Predicting image");
-        /*const prediction = await model.predict(webcam.canvas);
-        for (let i = 0; i < maxPredictions; i++) {
-            const classPrediction =
-                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
-        }*/
-        return "No idea";
+      } else {
+        extStatus = 1;
+        console.log("getUserMedia not supported");
+      }  
     }
+    
+    ext.stopWebcam = function() {
+      window.webcamStream.getVideoTracks().forEach(function(track) {
+        track.stop();
+      });
+    }
+    
+    ext.getCameraURL = function() {
+      window.canvas = document.createElement('canvas');
+      // Context object for working with the canvas.
+      window.ctx = canvas.getContext('2d');
 
+      // Get the exact size of the video element.
+      window.width = videoElem.videoWidth; // is there a size limit on tensorflow? dalton used 32
+      window.height = videoElem.videoHeight; // is there a size limit on tensorflow? dalton used 32
 
+      // Set the canvas to the same dimensions as the video.
+      canvas.width = width;
+      canvas.height = height;
 
+      // Draw a copy of the current frame from the video on the canvas.
+      ctx.drawImage(videoElem, 0, 0, width, height);
 
-
-	var descriptor = {
-
-	url: '', // update to something?
-
-        blocks: [
-      ['r', 'recognize camera image', 'predictImage'],
-      ['R', 'camera image', 'getCameraImage'],
-			]
-        //,menus: {}
+      // Get an image dataURL from the canvas.
+      var imageDataURL = canvas.toDataURL('image/png');
+      console.log('Image URL: ' + imageDataURL);
+      return imageDataURL;
+    }
+    
+    /*ext.callbackFunc = function (args callback) {
+      if (typeof callback=="function") callback();
+    };*/
+    
+    //ext.dataFunc = function () {return data;};
+    
+    ext._shutdown = function() {
+      ext.stopWebcam();
     };
 
-
-	ext._getStatus = function() {
-        var statusMsg;
-        if (mStatus == 0) {
-          statusMsg = ''
-        } else if (mStatus == 1) {
-          statusMsg = '';
-        } else {
-          statusMsg = 'Ready';
+    ext._getStatus = function() {
+        if (extStatus === 1) {
+            return {status: 1, msg: 'Your browser does not support webcam access.'};
         }
-        return {status: mStatus, msg:statusMsg};
-    };
-    
-  ext._stop = function() {
-  };  
-    
-	ext._shutdown = function() {
-	    if(poller) poller = clearInterval(poller);
-	    status = false;
-        ext.stopWebcam();
-	}
-
-  function getAppStatus() {
-          mStatus = 1;
-            ext.startImageWebcam();
- 
+        return {status: 2, msg: 'Ready'};
     };
 
-    getAppStatus();
+    var descriptor = {
+        blocks: [
+            ['r', 'recognize camera image', 'predictImage'],
+            ['r', 'get camera url', 'getCameraURL']
+        ],
+        menus: {}
+    };
+    
+    
+})();
+    
+  startImageWebcam().then(() => {
+        ScratchExtensions.register('PRG Tensorflow', descriptor, ext);;
+  });
+  
+  
+})();
 
-
-	ScratchExtensions.register('PRG Tensorflow', descriptor, ext);
-})({});
